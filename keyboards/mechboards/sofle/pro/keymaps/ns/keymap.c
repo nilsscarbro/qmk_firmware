968e16a3bbbec07db5f77bda1fb80b21d17793a2
Nils Scarbro <nilsscarbro@gmail.com>
2026-06-15 22:03:59 +0100

    Add custom keycodes and key overrides for enhanced functionality in Sofle Pro keymap

--- keyboards/mechboards/sofle/pro/keymaps/ns/keymap.c
+++ keyboards/mechboards/sofle/pro/keymaps/ns/keymap.c
@@ -3,6 +3,18 @@
 #include "config.h"
 #include "keymap_uk.h"
 
+#define HK_SNIP LGUI(LSFT(KC_S)) // Snipping Tool
+//type plain text ctrl + shift + v
+#define HK_PASTE LCTL(LSFT(KC_V))
+// ctrl + shift + home
+#define HK_START LCTL(LSFT(KC_HOME))
+// ctrl + shift + end
+#define HK_END LCTL(LSFT(KC_END))
+//  HotStringer
+#define HK_STRING LGUI(KC_INS)
+// Image paste
+#define HK_IMG_PASTE LCTL(LGUI(KC_INS))
+
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
@@ -10,8 +22,49 @@ const key_override_t *key_overrides[] = {
 	&delete_key_override
 };
 
+enum custom_keycodes {
+    SWAP_SP_ENT = SAFE_RANGE
+};
 
-#define HK_SNIP LGUI(LSFT(KC_S)) // Snipping Tool
+static bool sp_ent_swapped = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        // Handle the toggle key
+        case SWAP_SP_ENT:
+            if (record->event.pressed) {
+                sp_ent_swapped = !sp_ent_swapped;
+            }
+            return false; // Skip further processing for this key
+
+        // Intercept Space
+        case KC_SPC:
+            if (sp_ent_swapped) {
+                if (record->event.pressed) {
+                    register_code(KC_ENT); // Send Enter press instead [3]
+                } else {
+                    unregister_code(KC_ENT); // Send Enter release instead [3]
+                }
+                return false; // Prevent the original Space keycode from being sent [1]
+            }
+            break;
+
+        // Intercept Enter
+        case KC_ENT:
+            if (sp_ent_swapped) {
+                if (record->event.pressed) {
+                    register_code(KC_SPC); // Send Space press instead [3]
+                } else {
+                    unregister_code(KC_SPC); // Send Space release instead [3]
+                }
+                return false; // Prevent the original Enter keycode from being sent [1]
+            }
+            break;
+    }
+    return true; // Process all other keys normally [1]
+}
+
+
 
 // clang-format off
 
@@ -25,20 +78,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT(
     HK_SNIP, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,        _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_EQL,    KC_PLUS,    KC_MINUS,    KC_UNDS,    KC_F12,
+    _______, UK_TILD, UK_AT,   XXXXXXX, XXXXXXX,  XXXXXXX,                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, UK_PIPE,
+    _______, KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,        _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, UK_BSLS, _______,
                       _______, _______, _______, _______, _______,        _______, MO(3),   _______, _______, _______
   ),
   [2] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,                          KC_INS, _______, _______, _______, _______ ,_______,
+    _______, _______, _______, _______, _______, _______,                          KC_INS, _______, _______, _______, _______ ,LSFT(KC_DEL),
     _______, _______,  HK_SNIP, KC_APP,  XXXXXXX, XXXXXXX,                          KC_HOME, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_BSPC,
     _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_CAPS,                          KC_END, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  XXXXXXX,
     _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XXXXXXX, _______,        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
                       _______, _______, _______, MO(3),   _______,        _______, _______, _______, _______, _______
   ),
     [3] = LAYOUT(
-    XXXXXXX, XXXXXXX, XXXXXXX ,XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    QK_CLEAR_EEPROM, XXXXXXX, XXXXXXX ,XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, SWAP_SP_ENT,
     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                          XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
     RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX, XXXXXXX,        XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
